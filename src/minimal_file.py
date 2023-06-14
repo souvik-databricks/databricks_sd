476b79bb061be2d9ca4d420d99e0800ce2e75d38
christophergrant <christophergrant>
2023-06-14 22:20:53 +0000

    added payload generation and basic http hosting capability
    having trouble getting either the eventloop to run in a notebook and having issues with dbutils outside of the notebook

--- src/minimal_file.py
+++ src/minimal_file.py
@@ -1,17 +1,9 @@
-# Databricks notebook source
-# MAGIC %pip install databricks-sdk fastapi uvicorn
-
-# COMMAND ----------
-
 SD_TARGET_PORT = 8000
 
-# COMMAND ----------
 
 host = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiUrl().getOrElse(None)
 token = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().getOrElse(None)
 workspace_id = dbutils.notebook.entry_point.getDbutils().notebook().getContext().workspaceId().getOrElse(None)
-
-# COMMAND ----------
 
 def detect_cloud(host):
 
@@ -45,25 +37,6 @@ def generate_driver_proxy_url(host, workspace_id, cluster_id, port, endpoint):
     return new_url
 
 
-api_endpoint = generate_driver_proxy_url(
-    host, workspace_id, cluster_id, 40001, "metrics/json"
-)
-print(api_endpoint)
-
-# COMMAND ----------
-
-import requests
-
-url = api_endpoint
-
-headers = {"Authorization": f"Bearer {token}"}
-
-response = requests.get(url, headers=headers)
-
-print(response.json())
-
-# COMMAND ----------
-
 from databricks.sdk import WorkspaceClient
 from databricks.sdk.service.compute import State
 
@@ -77,13 +50,12 @@ w = WorkspaceClient(host=host, token=token)
 running_clusters = (c for c in w.clusters.list() if is_running_cluster(c))
 urls = (generate_driver_proxy_url(host=host, workspace_id=workspace_id, cluster_id=cluster.cluster_id, port=40001, endpoint="metrics/json") for cluster in running_clusters)
 
-# COMMAND ----------
 
 import json
 
-url_list = list(urls)  # Your list of URLs
+url_list = list(urls)
 
-label_dict = {}  # Your label dictionary
+label_dict = {}
 
 payload = [
     {
@@ -94,10 +66,7 @@ payload = [
 
 json_payload = json.dumps(payload)  # Convert the payload to a JSON string
 
-print(json_payload)
 
-
-# COMMAND ----------
 
 from fastapi import FastAPI
 from typing import List, Dict, Any
@@ -111,12 +80,3 @@ async def get_payload():
   
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=SD_TARGET_PORT)
-
-# COMMAND ----------
-
-# MAGIC %scala
-# MAGIC dbutils.notebook.getContext.workspaceId
-
-# COMMAND ----------
-
-
