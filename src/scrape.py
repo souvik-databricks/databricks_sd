a5c51e6848306ea97b3e964c14a4e5ab7c459e94
christophergrant <christophergrant>
2023-06-16 22:47:44 +0000

    cleanup on aisle 7

--- src/scrape.py
+++ src/scrape.py
@@ -115,6 +115,43 @@ from databricks.sdk import WorkspaceClient
 from databricks.sdk.service.compute import State
 import requests
 from requests.auth import HTTPBasicAuth
+import re
+
+def scrape_metrics():
+  url = f"https://{host}/api/2.0/cluster-metrics/metrics"
+
+  running_clusters = (c for c in w.clusters.list() if is_running_cluster(c))
+  cluster_ids = list(cluster.cluster_id for cluster in running_clusters)
+
+  data = {
+      "cluster_ids": cluster_ids,
+      "metric_names": []
+  }
+  headers = {"Authorization": f"Bearer {token}"}
+
+
+  response = requests.get(url, headers=headers, json=data)
+
+  text = response.text
+
+  # Split metrics into lines
+  lines = text.split('\n')
+
+  # Prepare pattern
+  pattern = re.compile('# TYPE (.+?) (.+?)')
+
+  # Transform lines using list comprehension
+  lines = [
+     re.sub('# TYPE (.+?) unknown', '# TYPE \\1 untyped', line)  # Change 'unknown' type to 'untyped'
+     for line in lines
+  ]
+
+  # Join lines back into a single string
+  metrics = '\n'.join(lines)
+
+  with open("metrics.txt", "w") as f:
+    f.write(metrics)
+    
 
 def is_running_cluster(cluster):
     return cluster.state in (
@@ -127,51 +164,9 @@ while True:
   running_clusters = (c for c in w.clusters.list() if is_running_cluster(c))
   targets = (generate_driver_proxy_url(context, cluster=cluster, port=40001) for cluster in running_clusters)
   output_to_json(list(targets))
+  scrape_metrics()
   time.sleep(POLLING_INTERVAL)  # Wait for 5 seconds
 
-
-# COMMAND ----------
-
-import re
-
-api_url = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiUrl().getOrElse(None)
-host = dbutils.notebook.entry_point.getDbutils().notebook().getContext().browserHostName().getOrElse(None)
-token = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().getOrElse(None)
-workspace_id = dbutils.notebook.entry_point.getDbutils().notebook().getContext().workspaceId().getOrElse(None)
-
-url = f"https://{host}/api/2.0/cluster-metrics/metrics"
-
-running_clusters = (c for c in w.clusters.list() if is_running_cluster(c))
-cluster_ids = list(cluster.cluster_id for cluster in running_clusters)
-
-data = {
-    "cluster_ids": cluster_ids,
-    "metric_names": []
-}
-headers = {"Authorization": f"Bearer {token}"}
-
-
-response = requests.get(url, headers=headers, json=data)
-
-text = response.text
-
-# Split metrics into lines
-lines = text.split('\n')
-
-# Prepare pattern
-pattern = re.compile('# TYPE (.+?) (.+?)')
-
-# Transform lines using list comprehension
-lines = [
-   re.sub('# TYPE (.+?) unknown', '# TYPE \\1 untyped', line)  # Change 'unknown' type to 'untyped'
-   for line in lines
-]
-
-# Join lines back into a single string
-metrics = '\n'.join(lines)
-
-with open("metrics.txt", "w") as f:
-  f.write(metrics)
 
 # COMMAND ----------
 
